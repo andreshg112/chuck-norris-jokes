e6dc72af90aa955aadc196910933b9d83578a9b8
Andrés Herrera García <andreshg112@gmail.com>
2019-04-19 16:52:31 -0500

    30 - Testing database migrations

--- tests/LaravelTest.php
+++ tests/LaravelTest.php
@@ -4,6 +4,7 @@ namespace Andreshg112\ChuckNorrisJokes\Tests;
 
 use Orchestra\Testbench\TestCase;
 use Illuminate\Support\Facades\Artisan;
+use Andreshg112\ChuckNorrisJokes\Models\Joke;
 use Andreshg112\ChuckNorrisJokes\Facades\ChuckNorris;
 use Andreshg112\ChuckNorrisJokes\Console\ChuckNorrisJoke;
 use Andreshg112\ChuckNorrisJokes\ChuckNorrisJokesServiceProvider;
@@ -12,7 +13,9 @@ class LaravelTest extends TestCase
 {
     protected function getPackageProviders($app)
     {
-        return [ChuckNorrisJokesServiceProvider::class];
+        return [
+            ChuckNorrisJokesServiceProvider::class,
+        ];
     }
 
     protected function getPackageAliases($app)
@@ -20,6 +23,13 @@ class LaravelTest extends TestCase
         return [
             'ChuckNorris' => ChuckNorrisJoke::class,
         ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        include_once __DIR__ . '/../database/migrations/create_jokes_table.php.stub';
+
+        (new \CreateJokesTable)->up();
     }
 
     /** @test */
@@ -35,7 +45,7 @@ class LaravelTest extends TestCase
 
         $output = Artisan::output();
 
-        $this->assertSame('some joke'.PHP_EOL, $output);
+        $this->assertSame('some joke' . PHP_EOL, $output);
     }
 
     /** @test */
@@ -49,5 +59,17 @@ class LaravelTest extends TestCase
             ->assertViewIs('chuck-norris::joke')
             ->assertViewHas('joke', 'some joke')
             ->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_can_access_the_database()
+    {
+        $joke = new Joke();
+        $joke->joke = 'this is funny';
+        $joke->save();
+
+        $newJoke = Joke::find($joke->id);
+
+        $this->assertSame($newJoke->joke, 'this is funny');
     }
 }
